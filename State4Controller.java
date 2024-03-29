e2904bb601caac7b28b48258b4d813943264d0d5
jeckschmidt <45171254+jeckschmidt@users.noreply.github.com>
2024-03-28 22:59:57 -0400

    Add files via upload

---
+++ State4Controller.java
@@ -0,0 +1,75 @@
+package application;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
+
+import javafx.animation.AnimationTimer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+public class State4Controller implements Initializable{
+	
+	// import methods from GlobalController as global
+	GlobalController global = new GlobalController();
+	
+	
+	@FXML private Text dateTime;
+	@FXML private Button logoutButton;
+	@FXML private AnchorPane anchorPane;
+	Stage stage;
+
+	
+	public void switchToState2(ActionEvent e) throws IOException{
+		 global.switchToState2(e);
+	}
+		
+	
+	public void switchToState5(ActionEvent e) throws IOException{
+		global.switchToState5(e);
+		
+	}
+	
+	public void logout(ActionEvent e){
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Logout");
+		alert.setHeaderText("You're about to logout!");
+		alert.setContentText("Are you sure you want to leave?");
+		
+		if (alert.showAndWait().get() == ButtonType.OK) {
+			stage = (Stage) anchorPane.getScene().getWindow();
+			System.out.println("logged out");
+			stage.close();
+		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// start the animation timer so the date will be dynamically updated every second
+		timer.start();
+		
+	}
+
+	// create an animation object
+	AnimationTimer timer = new AnimationTimer() {
+		@Override
+	    public void handle(long now) {
+			// set the dateTime Text to the current time with custom formatting
+	        dateTime.setText(LocalDateTime.now().format(DateTimeFormatter.ofPattern("MMM-dd-yyyy HH:mm:ss")));
+	    }
+	};
+}
---
+++ groceryItem.java
@@ -0,0 +1,23 @@
+package application;
+
+
+public class groceryItem {
+
+	// object attributes
+	private int ID;
+	private float cost;
+	
+	
+	// get methods
+	public int getID() {
+		return this.ID;
+	}
+	
+	
+	public float getCost() {
+		return this.cost;
+	}
+	
+
+	
+}
